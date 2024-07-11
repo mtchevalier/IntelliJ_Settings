ecfbb6d05d2e475a580678013146321ca545fdf0
matth <matth@Calcipher>
2024-07-11 12:25:13 -0400

    IU-2024.1.1 <matth@Calcipher Update Lightning Web Component JavaScript.js

--- fileTemplates/internal/Lightning Web Component JavaScript.js
+++ fileTemplates/internal/Lightning Web Component JavaScript.js
@@ -1,4 +1,6 @@
 import { LightningElement } from 'lwc';
+import {ShowToastEvent} from "lightning/platformShowToastEvent";
+import {reduceErrors} from "c/utils";
 
 export default class ${CLASS_NAME} extends LightningElement {
 
